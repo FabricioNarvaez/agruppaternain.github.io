4f4aaf8117254ca336e4364910a47b02c7078d93
FabricioNarvaez <narvaezfabricio38@gmail.com>
2023-03-29 20:35:11 +0200

    Added second table

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,83 +1,82 @@
-let grupoA = [
-  { equipo: "Los Panas", PJ: 10, PG: 8, PE: 1, PP: 3, GF: 10, GC: 5, GD: 0, Pts: 0 },
-  { equipo: "Peñarol", PJ: 10, PG: 7, PE: 1, PP: 3, GF: 8, GC: 5, GD: 0, Pts: 0 },
-  { equipo: "Comboloco", PJ: 10, PG: 6, PE: 1, PP: 5, GF: 5, GC: 5, GD: 0, Pts: 0 },
-  { equipo: "Los Rumberos", PJ: 10, PG: 6, PE: 1, PP: 5, GF: 5, GC: 5, GD: 0, Pts: 0 },
-  { equipo: "Latin Brothers", PJ: 10, PG: 6, PE: 1, PP: 5, GF: 5, GC: 5, GD: 0, Pts: 0 },
-  { equipo: "Golden", PJ: 10, PG: 6, PE: 1, PP: 5, GF: 5, GC: 5, GD: 0, Pts: 0 }
+let groupA = [
+  { team: "Los Panas", PJ: 10, PG: 8, PE: 1, PP: 3, GF: 10, GC: 5, GD: 0, Pts: 0 },
+  { team: "Peñarol", PJ: 10, PG: 7, PE: 1, PP: 3, GF: 8, GC: 5, GD: 0, Pts: 0 },
+  { team: "Comboloco", PJ: 10, PG: 6, PE: 1, PP: 5, GF: 5, GC: 5, GD: 0, Pts: 0 },
+  { team: "Los Rumberos", PJ: 10, PG: 6, PE: 1, PP: 5, GF: 5, GC: 5, GD: 0, Pts: 0 },
+  { team: "Latin Brothers", PJ: 10, PG: 6, PE: 1, PP: 5, GF: 5, GC: 5, GD: 0, Pts: 0 },
+  { team: "Golden", PJ: 10, PG: 6, PE: 1, PP: 5, GF: 5, GC: 5, GD: 0, Pts: 0 }
 ];
 
 // let grupoB = [
-//   { equipo: "Impersiva", PJ: 10, PG: 8, PE: 1, PP: 3, GF: 10, GC: 5, GD: 0, Pts: 0 },
-//   { equipo: "Estella", PJ: 10, PG: 7, PE: 1, PP: 3, GF: 8, GC: 5, GD: 0, Pts: 0 },
-//   { equipo: "Cancheritos", PJ: 10, PG: 6, PE: 1, PP: 5, GF: 5, GC: 5, GD: 0, Pts: 0 },
-//   { equipo: "El Valle", PJ: 10, PG: 6, PE: 1, PP: 5, GF: 5, GC: 5, GD: 0, Pts: 0 },
-//   { equipo: "Spencer", PJ: 10, PG: 6, PE: 1, PP: 5, GF: 5, GC: 5, GD: 0, Pts: 0 },
-//   { equipo: "Amistad", PJ: 10, PG: 6, PE: 1, PP: 5, GF: 5, GC: 5, GD: 0, Pts: 0 }
+//   { team: "Impersiva", PJ: 10, PG: 8, PE: 1, PP: 3, GF: 10, GC: 5, GD: 0, Pts: 0 },
+//   { team: "Estella", PJ: 10, PG: 7, PE: 1, PP: 3, GF: 8, GC: 5, GD: 0, Pts: 0 },
+//   { team: "Cancheritos", PJ: 10, PG: 6, PE: 1, PP: 5, GF: 5, GC: 5, GD: 0, Pts: 0 },
+//   { team: "El Valle", PJ: 10, PG: 6, PE: 1, PP: 5, GF: 5, GC: 5, GD: 0, Pts: 0 },
+//   { team: "Spencer", PJ: 10, PG: 6, PE: 1, PP: 5, GF: 5, GC: 5, GD: 0, Pts: 0 },
+//   { team: "Amistad", PJ: 10, PG: 6, PE: 1, PP: 5, GF: 5, GC: 5, GD: 0, Pts: 0 }
 // ];
 
-let cuerpoTabla = document.querySelector("tbody");
+let bodyTableA = document.getElementById("groupA");
 
-function procesarGrupo(equipo, index) {
-  console.log(index)
-    let fila = document.createElement("tr");
+function processGroup(team, index) {
+    let row = document.createElement("tr");
 
     let celdaPos = document.createElement("td");
     celdaPos.textContent = index + 1;
-    fila.appendChild(celdaPos);
+    row.appendChild(celdaPos);
 
-    let celdaEquipo = document.createElement("td");
-    celdaEquipo.textContent = equipo.equipo;
-    fila.appendChild(celdaEquipo);
+    let celdaTeam = document.createElement("td");
+    celdaTeam.textContent = team.team;
+    row.appendChild(celdaTeam);
 
     let celdaPJ = document.createElement("td");
-    celdaPJ.textContent = equipo.PJ;
-    fila.appendChild(celdaPJ);
+    celdaPJ.textContent = team.PJ;
+    row.appendChild(celdaPJ);
     
     let celdaPG = document.createElement("td");
-    celdaPG.textContent = equipo.PG;
-    fila.appendChild(celdaPG);
+    celdaPG.textContent = team.PG;
+    row.appendChild(celdaPG);
 
     let celdaPE = document.createElement("td");
-    celdaPE.textContent = equipo.PE;
-    fila.appendChild(celdaPE);
+    celdaPE.textContent = team.PE;
+    row.appendChild(celdaPE);
     
     let celdaPP = document.createElement("td");
-    celdaPP.textContent = equipo.PP;
-    fila.appendChild(celdaPP);
+    celdaPP.textContent = team.PP;
+    row.appendChild(celdaPP);
 
     let celdaGF = document.createElement("td");
-    celdaGF.textContent = equipo.GF;
-    fila.appendChild(celdaGF);
+    celdaGF.textContent = team.GF;
+    row.appendChild(celdaGF);
     
     let celdaGC = document.createElement("td");
-    celdaGC.textContent = equipo.PG;
-    fila.appendChild(celdaGC);
+    celdaGC.textContent = team.PG;
+    row.appendChild(celdaGC);
 
     let celdaGD = document.createElement("td");
-    celdaGD.textContent = equipo.GD;
-    fila.appendChild(celdaGD);
+    celdaGD.textContent = team.GD;
+    row.appendChild(celdaGD);
 
     let celdaPuntos = document.createElement("td");
-    celdaPuntos.textContent = equipo.Pts;
-    fila.appendChild(celdaPuntos);
+    celdaPuntos.textContent = team.Pts;
+    row.appendChild(celdaPuntos);
 
 
-    cuerpoTabla.appendChild(fila);
+    bodyTableA.appendChild(row);
 };
 
-function calcularPuntos(equipo){
-    let puntos = equipo.PG * 3;
-    if(equipo.PE){
-        puntos += equipo.PE;
+function calculatePoints(team){
+    let puntos = team.PG * 3;
+    if(team.PE){
+        puntos += team.PE;
     }
-    equipo.Pts = puntos;
+    team.Pts = puntos;
 
-    let gd = equipo.GF - equipo.GC;
-    equipo.GD = gd > 0 ? `+${gd}` : gd;
+    let gd = team.GF - team.GC;
+    team.GD = gd > 0 ? `+${gd}` : gd;
 }
 
-function ordenarPorPuntosYGoles(a, b) {
+function sortByPointsAndGoals(a, b) {
     if (a.Pts > b.Pts) {
       return -1;
     } else if (a.Pts < b.Pts) {
@@ -93,6 +92,6 @@ function ordenarPorPuntosYGoles(a, b) {
     }
   }
 
-grupoA.forEach(calcularPuntos);
-grupoA.sort(ordenarPorPuntosYGoles);
-grupoA.forEach(procesarGrupo);
+groupA.forEach(calculatePoints);
+groupA.sort(sortByPointsAndGoals);
+groupA.forEach(processGroup);
