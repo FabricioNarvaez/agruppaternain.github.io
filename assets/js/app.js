2b8788d37b35326f7ca3514add0571a1f05b540b
FabricioNarvaez <narvaezfabricio38@gmail.com>
2023-03-29 21:17:47 +0200

    Used real data

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,19 @@
 let groupA = [
-  { team: "Los Panas", PJ: 10, PG: 8, PE: 1, PP: 3, GF: 10, GC: 5, GD: 0, Pts: 0 },
-  { team: "Peñarol", PJ: 10, PG: 7, PE: 1, PP: 3, GF: 8, GC: 5, GD: 0, Pts: 0 },
-  { team: "Comboloco", PJ: 10, PG: 6, PE: 1, PP: 5, GF: 5, GC: 5, GD: 0, Pts: 0 },
-  { team: "Los Rumberos", PJ: 10, PG: 6, PE: 1, PP: 5, GF: 5, GC: 5, GD: 0, Pts: 0 },
-  { team: "Latin Brothers", PJ: 10, PG: 6, PE: 1, PP: 5, GF: 5, GC: 5, GD: 0, Pts: 0 },
-  { team: "Golden", PJ: 10, PG: 6, PE: 1, PP: 5, GF: 5, GC: 5, GD: 0, Pts: 0 }
+  { team: "Los Panas",      PJ: 0, PG: 2, PE: 0, PP: 0, GF: 16, GC: 4,  GD: 0, Pts: 0 },
+  { team: "Peñarol",        PJ: 0, PG: 1, PE: 0, PP: 1, GF: 6,  GC: 7,  GD: 0, Pts: 0 },
+  { team: "Comboloco",      PJ: 0, PG: 1, PE: 0, PP: 0, GF: 5,  GC: 0,  GD: 0, Pts: 0 },
+  { team: "Los Rumberos",   PJ: 0, PG: 0, PE: 0, PP: 1, GF: 0,  GC: 15, GD: 0, Pts: 0 },
+  { team: "Latin Brothers", PJ: 0, PG: 1, PE: 0, PP: 0, GF: 6,  GC: 0,  GD: 0, Pts: 0 },
+  { team: "Golden",         PJ: 0, PG: 0, PE: 0, PP: 2, GF: 1,  GC: 8,  GD: 0, Pts: 0 }
 ];
 
 let grupoB = [
-  { team: "Impersiva", PJ: 10, PG: 8, PE: 1, PP: 3, GF: 10, GC: 5, GD: 0, Pts: 0 },
-  { team: "Estella", PJ: 10, PG: 7, PE: 1, PP: 3, GF: 8, GC: 5, GD: 0, Pts: 0 },
-  { team: "Cancheritos", PJ: 10, PG: 6, PE: 1, PP: 5, GF: 5, GC: 5, GD: 0, Pts: 0 },
-  { team: "El Valle", PJ: 10, PG: 6, PE: 1, PP: 5, GF: 5, GC: 5, GD: 0, Pts: 0 },
-  { team: "Spencer", PJ: 10, PG: 6, PE: 1, PP: 5, GF: 5, GC: 5, GD: 0, Pts: 0 },
-  { team: "Amistad", PJ: 10, PG: 6, PE: 1, PP: 5, GF: 5, GC: 5, GD: 0, Pts: 0 }
+  { team: "Impersiva",      PJ: 0, PG: 1, PE: 0, PP: 1, GF: 4,  GC: 5,  GD: 0, Pts: 0 },
+  { team: "Estella",        PJ: 0, PG: 0, PE: 1, PP: 1, GF: 2,  GC: 3,  GD: 0, Pts: 0 },
+  { team: "Cancheritos",    PJ: 0, PG: 0, PE: 1, PP: 1, GF: 1,  GC: 6,  GD: 0, Pts: 0 },
+  { team: "El Valle",       PJ: 0, PG: 1, PE: 0, PP: 1, GF: 7,  GC: 3,  GD: 0, Pts: 0 },
+  { team: "Spencer",        PJ: 0, PG: 2, PE: 0, PP: 0, GF: 8,  GC: 3,  GD: 0, Pts: 0 },
+  { team: "Amistad",        PJ: 0, PG: 1, PE: 0, PP: 1, GF: 4,  GC: 6,  GD: 0, Pts: 0 }
 ];
 
 function createRow(team, index){
@@ -48,7 +48,7 @@ function createRow(team, index){
     row.appendChild(celdaGF);
     
     let celdaGC = document.createElement("td");
-    celdaGC.textContent = team.PG;
+    celdaGC.textContent = team.GC;
     row.appendChild(celdaGC);
 
     let celdaGD = document.createElement("td");
@@ -72,7 +72,7 @@ function processGroupB(team, index) {
     bodyTableB.appendChild(createRow(team, index));
 };
 
-function calculatePoints(team){
+function calculatePointsGoalsMatches(team){
     let puntos = team.PG * 3;
     if(team.PE){
         puntos += team.PE;
@@ -81,6 +81,8 @@ function calculatePoints(team){
 
     let gd = team.GF - team.GC;
     team.GD = gd > 0 ? `+${gd}` : gd;
+
+    team.PJ = team.PG + team.PE + team.PP;
 }
 
 function sortByPointsAndGoals(a, b) {
@@ -99,10 +101,10 @@ function sortByPointsAndGoals(a, b) {
     }
   }
 
-groupA.forEach(calculatePoints);
+groupA.forEach(calculatePointsGoalsMatches);
 groupA.sort(sortByPointsAndGoals);
 groupA.forEach(processGroupA);
 
-grupoB.forEach(calculatePoints);
+grupoB.forEach(calculatePointsGoalsMatches);
 grupoB.sort(sortByPointsAndGoals);
 grupoB.forEach(processGroupB);
