f367e9517405d1c83e385f2d323a13f7f716a127
FabricioNarvaez <narvaezfabricio38@gmail.com>
2023-04-20 12:18:32 +0200

    Updated data for group A

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,10 @@
 let groupA = [
-  { team: "Los Panas",      PG: 3, PE: 0, PP: 0, GF: 20, GC: 5},
-  { team: "Peñarol",        PG: 1, PE: 0, PP: 2, GF: 8,  GC: 12},
-  { team: "Comboloco",      PG: 2, PE: 0, PP: 0, GF: 10,  GC: 2},
-  { team: "Los Rumberos",   PG: 1, PE: 0, PP: 2, GF: 10,  GC: 15},
-  { team: "Latin Brothers", PG: 1, PE: 0, PP: 1, GF: 7,  GC: 4},
-  { team: "Golden",         PG: 0, PE: 0, PP: 3, GF: 1,  GC: 18}
+  { team: "Los Panas",      PG: 4, PE: 0, PP: 0, GF: 23, GC: 6},
+  { team: "Peñarol",        PG: 2, PE: 0, PP: 2, GF: 19,  GC: 15},
+  { team: "Comboloco",      PG: 3, PE: 0, PP: 0, GF: 14,  GC: 3},
+  { team: "Los Rumberos",   PG: 1, PE: 0, PP: 3, GF: 13,  GC: 26},
+  { team: "Latin Brothers", PG: 1, PE: 0, PP: 2, GF: 8,  GC: 8},
+  { team: "Golden",         PG: 0, PE: 0, PP: 4, GF: 2,  GC: 22}
 ];
 
 let grupoB = [
