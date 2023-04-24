cce64059f41b647240fee945c1b16cadcf3a98b3
FabricioNarvaez <narvaezfabricio38@gmail.com>
2023-04-24 19:16:12 +0200

    Added triangles to indicate changing positions

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,19 @@
 let groupA = [
-  { team: "Los Panas",      PG: 4, PE: 0, PP: 0, GF: 23,  GC: 6},
-  { team: "Peñarol",        PG: 2, PE: 0, PP: 3, GF: 21,  GC: 21},
-  { team: "Comboloco",      PG: 4, PE: 0, PP: 0, GF: 22,  GC: 3},
-  { team: "Los Rumberos",   PG: 1, PE: 0, PP: 3, GF: 13,  GC: 26},
-  { team: "Latin Brothers", PG: 2, PE: 0, PP: 2, GF: 14,   GC: 10},
-  { team: "Golden",         PG: 0, PE: 0, PP: 5, GF: 2,   GC: 30}
+  { team: "Los Panas",      PG: 4, PE: 0, PP: 0, GF: 23,  GC: 6, lastPos: 1},
+  { team: "Peñarol",        PG: 2, PE: 0, PP: 3, GF: 21,  GC: 21, lastPos: 3},
+  { team: "Comboloco",      PG: 4, PE: 0, PP: 0, GF: 22,  GC: 3, lastPos: 2},
+  { team: "Los Rumberos",   PG: 1, PE: 0, PP: 3, GF: 13,  GC: 26, lastPos: 5},
+  { team: "Latin Brothers", PG: 2, PE: 0, PP: 2, GF: 14,   GC: 10, lastPos: 4},
+  { team: "Golden",         PG: 0, PE: 0, PP: 5, GF: 2,   GC: 30, lastPos: 6}
 ];
 
 let grupoB = [
-  { team: "Impersiva",      PG: 2, PE: 0, PP: 3, GF: 9,   GC: 14},
-  { team: "Estella",        PG: 0, PE: 2, PP: 3, GF: 5,   GC: 10},
-  { team: "Cancheritos",    PG: 0, PE: 1, PP: 4, GF: 7,   GC: 16},
-  { team: "El Valle",       PG: 3, PE: 0, PP: 2, GF: 15,  GC: 8},
-  { team: "Spencer",        PG: 4, PE: 1, PP: 0, GF: 15,   GC: 7},
-  { team: "Amistad",        PG: 4, PE: 0, PP: 1, GF: 13,   GC: 9}
+  { team: "Impersiva",      PG: 2, PE: 0, PP: 3, GF: 9,   GC: 14, lastPos: 4},
+  { team: "Estella",        PG: 0, PE: 2, PP: 3, GF: 5,   GC: 10, lastPos: 5},
+  { team: "Cancheritos",    PG: 0, PE: 1, PP: 4, GF: 7,   GC: 16, lastPos: 6},
+  { team: "El Valle",       PG: 3, PE: 0, PP: 2, GF: 15,  GC: 8, lastPos: 3},
+  { team: "Spencer",        PG: 4, PE: 1, PP: 0, GF: 15,   GC: 7, lastPos: 1},
+  { team: "Amistad",        PG: 4, PE: 0, PP: 1, GF: 13,   GC: 9, lastPos: 2}
 ];
 
 function createRow(team, index){
@@ -29,7 +29,57 @@ function createRow(team, index){
     row.appendChild(celdaPos);
 
     let celdaTeam = document.createElement("td");
-    celdaTeam.textContent = team.team;
+    let difPos = team.lastPos - pos;
+    let leftText = document.createElement("span");
+    leftText.textContent = team.team;
+    leftText.style.display = "inline-block";
+    leftText.style.width = "50%";
+    leftText.style.textAlign = "left";
+
+    let rightText = document.createElement("span");
+    if(difPos < 0){
+      let triangulo = document.createElement("span");
+      triangulo.style.display = "inline-block";
+      triangulo.style.width = "0";
+      triangulo.style.height = "0";
+      triangulo.style.borderLeft = "5px solid transparent";
+      triangulo.style.borderRight = "5px solid transparent";
+      triangulo.style.borderTop = "5px solid red";
+      triangulo.style.marginRight = "5px";
+
+      let difPosSpan = document.createElement("span");
+      difPosSpan.textContent = `${Math.abs(difPos)}`;
+      difPosSpan.style.marginRight = "5px";
+
+
+      rightText.appendChild(triangulo);
+      rightText.appendChild(difPosSpan);
+      rightText.style.color = "red";
+    }else if(difPos > 0){
+      let triangulo = document.createElement("span");
+      triangulo.style.display = "inline-block";
+      triangulo.style.width = "0";
+      triangulo.style.height = "0";
+      triangulo.style.borderLeft = "5px solid transparent";
+      triangulo.style.borderRight = "5px solid transparent";
+      triangulo.style.borderBottom = "5px solid green";
+      triangulo.style.marginRight = "5px";
+
+      let difPosSpan = document.createElement("span");
+      difPosSpan.textContent = `${Math.abs(difPos)}`;
+      difPosSpan.style.marginRight = "5px";
+
+
+      rightText.appendChild(triangulo);
+      rightText.appendChild(difPosSpan);
+      rightText.style.color = "green";
+    }
+
+    rightText.style.display = "inline-block";
+    rightText.style.width = "50%";
+    rightText.style.textAlign = "right";
+    celdaTeam.appendChild(leftText);
+    celdaTeam.appendChild(rightText);
     row.appendChild(celdaTeam);
 
     let celdaPJ = document.createElement("td");
