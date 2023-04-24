e5430197d853cfb153319126537e9ab9b75a268f
FabricioNarvaez <narvaezfabricio38@gmail.com>
2023-04-24 18:07:15 +0200

    Added new data for Comboloco and Golden

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 let groupA = [
   { team: "Los Panas",      PG: 4, PE: 0, PP: 0, GF: 23,  GC: 6},
   { team: "Peñarol",        PG: 2, PE: 0, PP: 3, GF: 21,  GC: 21},
-  { team: "Comboloco",      PG: 3, PE: 0, PP: 0, GF: 14,  GC: 3},
+  { team: "Comboloco",      PG: 4, PE: 0, PP: 0, GF: 22,  GC: 3},
   { team: "Los Rumberos",   PG: 1, PE: 0, PP: 3, GF: 13,  GC: 26},
   { team: "Latin Brothers", PG: 2, PE: 0, PP: 2, GF: 14,   GC: 10},
   { team: "Golden",         PG: 0, PE: 0, PP: 4, GF: 2,   GC: 22}
