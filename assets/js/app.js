677e298fd7c169e8f78630f88358b100c766c134
FabricioNarvaez <narvaezfabricio38@gmail.com>
2023-04-27 18:49:09 +0200

    Replaced the use of let with const and add class to last column of table rows

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-let groupA = [
+const groupA = [
   { team: "Los Panas",      PG: 4, PE: 0, PP: 0, GF: 23,  GC: 6, lastPos: 1},
   { team: "Peñarol",        PG: 2, PE: 0, PP: 3, GF: 21,  GC: 21, lastPos: 3},
   { team: "Comboloco",      PG: 4, PE: 0, PP: 0, GF: 22,  GC: 3, lastPos: 2},
@@ -7,7 +7,7 @@ let groupA = [
   { team: "Golden",         PG: 0, PE: 0, PP: 5, GF: 2,   GC: 30, lastPos: 6}
 ];
 
-let grupoB = [
+const grupoB = [
   { team: "Impersiva",      PG: 2, PE: 0, PP: 3, GF: 9,   GC: 14, lastPos: 4},
   { team: "Estella",        PG: 0, PE: 2, PP: 3, GF: 5,   GC: 10, lastPos: 5},
   { team: "Cancheritos",    PG: 0, PE: 1, PP: 4, GF: 7,   GC: 16, lastPos: 6},
@@ -17,10 +17,10 @@ let grupoB = [
 ];
 
 function createRow(team, index){
-    let row = document.createElement("tr");
+    const row = document.createElement("tr");
 
-    let celdaPos = document.createElement("td");
-    let pos = index + 1;
+    const celdaPos = document.createElement("td");
+    const pos = index + 1;
     if(pos <= 2){
       celdaPos.style.borderLeft = "4px solid";
       celdaPos.style.borderLeftColor = "#0b95dd";
@@ -28,24 +28,24 @@ function createRow(team, index){
     celdaPos.textContent = pos;
     row.appendChild(celdaPos);
 
-    let celdaTeam = document.createElement("td");
+    const celdaTeam = document.createElement("td");
     celdaTeam.style.width = "280px";
-    let difPos = team.lastPos - pos;
-    let leftText = document.createElement("span");
+    const difPos = team.lastPos - pos;
+    const leftText = document.createElement("span");
     leftText.textContent = team.team;
     leftText.style.display = "inline-block";
     leftText.style.width = "50%";
     leftText.style.textAlign = "left";
 
-    let rightText = document.createElement("span");
-    let triangle = document.createElement("span");
+    const rightText = document.createElement("span");
+    const triangle = document.createElement("span");
     triangle.style.display = "inline-block";
     triangle.style.width = "0";
     triangle.style.height = "0";
     triangle.style.borderLeft = "5px solid transparent";
     triangle.style.borderRight = "5px solid transparent";
     triangle.style.marginRight = "5px";
-    let difPosSpan = document.createElement("span");
+    const difPosSpan = document.createElement("span");
     difPosSpan.style.marginRight = "5px";
     if(difPos < 0){
       triangle.style.borderTop = "5px solid red";
@@ -65,31 +65,31 @@ function createRow(team, index){
     celdaTeam.appendChild(rightText);
     row.appendChild(celdaTeam);
 
-    let celdaPJ = document.createElement("td");
+    const celdaPJ = document.createElement("td");
     celdaPJ.textContent = team.PJ;
     row.appendChild(celdaPJ);
     
-    let celdaPG = document.createElement("td");
+    const celdaPG = document.createElement("td");
     celdaPG.textContent = team.PG;
     row.appendChild(celdaPG);
 
-    let celdaPE = document.createElement("td");
+    const celdaPE = document.createElement("td");
     celdaPE.textContent = team.PE;
     row.appendChild(celdaPE);
     
-    let celdaPP = document.createElement("td");
+    const celdaPP = document.createElement("td");
     celdaPP.textContent = team.PP;
     row.appendChild(celdaPP);
 
-    let celdaGF = document.createElement("td");
+    const celdaGF = document.createElement("td");
     celdaGF.textContent = team.GF;
     row.appendChild(celdaGF);
     
-    let celdaGC = document.createElement("td");
+    const celdaGC = document.createElement("td");
     celdaGC.textContent = team.GC;
     row.appendChild(celdaGC);
 
-    let celdaGD = document.createElement("td");
+    const celdaGD = document.createElement("td");
     celdaGD.textContent = team.GD;
     celdaGD.style.fontWeight = "bold";
     if(team.GD > 0){
@@ -99,31 +99,29 @@ function createRow(team, index){
     }
     row.appendChild(celdaGD);
 
-    let celdaPuntos = document.createElement("td");
-    celdaPuntos.textContent = team.Pts;
-    celdaPuntos.style.fontWeight = "bold";
-    celdaPuntos.style.color = "white";
-    celdaPuntos.style.backgroundColor = "#818181";
-    row.appendChild(celdaPuntos);
+    const pointsField = document.createElement("td");
+    pointsField.textContent = team.Pts;
+    pointsField.classList.add("lastColumnRows");
+    row.appendChild(pointsField);
 
     return row;
 }
 
 function processGroupA(team, index) {
-    let bodyTableA = document.getElementById("groupA");
+    const bodyTableA = document.getElementById("groupA");
     bodyTableA.appendChild(createRow(team, index));
 };
 
 function processGroupB(team, index) {
-    let bodyTableB = document.getElementById("groupB");
+    const bodyTableB = document.getElementById("groupB");
     bodyTableB.appendChild(createRow(team, index));
 };
 
 function calculatePointsGoalsMatches(team){
-    let puntos = team.PG * 3;
+    const puntos = team.PG * 3;
     team.Pts = team.PE ? (puntos + team.PE) : puntos;
 
-    let gd = team.GF - team.GC;
+    const gd = team.GF - team.GC;
     team.GD = gd > 0 ? `+${gd}` : gd;
 
     team.PJ = team.PG + team.PE + team.PP;
--- assets/js/scorer.js
+++ assets/js/scorer.js
@@ -19,10 +19,10 @@ function sortByGoals(a, b){
 scorers.sort(sortByGoals);
 scorers.forEach((scorer, index) => {
     const scorrersTable = document.getElementById("scorersTable");
-    let row = document.createElement("tr");
+    const row = document.createElement("tr");
 
-    let celdaPos = document.createElement("td");
-    let pos = index + 1;
+    const celdaPos = document.createElement("td");
+    const pos = index + 1;
     if(pos === 1){
         celdaPos.style.background = "url(https://e00-marca.uecdn.es/assets/v31/img/estaticas/trofeos/first-mobile.svg) no-repeat center center";
         celdaPos.style.backgroundSize = "auto 100%";
@@ -31,20 +31,18 @@ scorers.forEach((scorer, index) => {
     }
     row.appendChild(celdaPos);
 
-    let celdaPlayer = document.createElement("td");
+    const celdaPlayer = document.createElement("td");
     celdaPlayer.textContent = scorer.name;
     row.appendChild(celdaPlayer);
 
-    let celdaTeam = document.createElement("td");
+    const celdaTeam = document.createElement("td");
     celdaTeam.textContent = scorer.team;
     row.appendChild(celdaTeam);
 
-    let celdaGoals = document.createElement("td");
-    celdaGoals.textContent = scorer.goals;
-    celdaGoals.style.fontWeight = "bold";
-    celdaGoals.style.color = "white";
-    celdaGoals.style.backgroundColor = "#818181";
-    row.appendChild(celdaGoals);
+    const goalsField = document.createElement("td");
+    goalsField.textContent = scorer.goals;
+    goalsField.classList.add("lastColumnRows");
+    row.appendChild(goalsField);
 
     scorrersTable.appendChild(row);
 });
