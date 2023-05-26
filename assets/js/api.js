ba6c09cfae798d9c65b4f92aa17305fe9f4b1b86
FabricioNarvaez <narvaezfabricio38@gmail.com>
2023-05-26 20:55:41 +0200

    Added loaders on index

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,10 +1,18 @@
 import { calculatePointsGoalsMatches, processGroupA, processGroupB, sortByPointsAndGoals} from './app.js';
 import { url } from './url.js';
 
+const tableBodyA = document.getElementById('groupA');
+const tableBodyB = document.getElementById('groupB');
+const loaderRowA = document.getElementById('loader-rowA');
+const loaderRowB = document.getElementById('loader-rowB');
+
 (async ()=> {
     
     const response = await fetch(`${url}api/teams`);
     const {teamsGroupA, teamsGroupB} = await response.json();
+    tableBodyA.removeChild(loaderRowA);
+    tableBodyB.removeChild(loaderRowB);
+
     teamsGroupA.forEach(calculatePointsGoalsMatches);
     teamsGroupA.sort(sortByPointsAndGoals);
     teamsGroupA.forEach(processGroupA);
