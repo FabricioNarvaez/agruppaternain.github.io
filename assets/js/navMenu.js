68bd4dfde71a9efca28ee37d75ef5482e3690e0f
FabricioNarvaez <narvaezfabricio38@gmail.com>
2023-06-02 19:03:02 +0200

    Close nav when nav link is clicked

--- assets/js/navMenu.js
+++ assets/js/navMenu.js
@@ -5,3 +5,8 @@ menuIcon.addEventListener("click", ()=>{
     menuIcon.classList.toggle("activated");
     navMenu.classList.toggle("activated");
 })
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", ()=> {
+    menuIcon.classList.remove("activated");
+    navMenu.classList.remove("activated");
+}))
