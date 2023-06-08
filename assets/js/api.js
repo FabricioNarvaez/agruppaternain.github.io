c2f44bc538dc389a4bf5143c25a7151ae4664b66
FabricioNarvaez <narvaezfabricio38@gmail.com>
2023-06-08 20:11:47 +0200

    Formated api.js with prettier

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,22 +1,26 @@
-import { calculatePointsGoalsMatches, processGroupA, processGroupB, sortByPointsAndGoals} from './groupsTables.js';
-import { url } from './url.js';
+import {
+    calculatePointsGoalsMatches,
+    processGroupA,
+    processGroupB,
+    sortByPointsAndGoals,
+} from "./groupsTables.js";
+import { url } from "./url.js";
 
-const tableBodyA = document.getElementById('groupA');
-const tableBodyB = document.getElementById('groupB');
-const loaderRowA = document.getElementById('loader-rowA');
-const loaderRowB = document.getElementById('loader-rowB');
+const tableBodyA = document.getElementById("groupA");
+const tableBodyB = document.getElementById("groupB");
+const loaderRowA = document.getElementById("loader-rowA");
+const loaderRowB = document.getElementById("loader-rowB");
 
-(async ()=> {
-    
+(async () => {
     const response = await fetch(`${url}api/teams`);
-    const {teamsGroupA, teamsGroupB} = await response.json();
+    const { teamsGroupA, teamsGroupB } = await response.json();
     tableBodyA.removeChild(loaderRowA);
     tableBodyB.removeChild(loaderRowB);
 
     teamsGroupA.forEach(calculatePointsGoalsMatches);
     teamsGroupA.sort(sortByPointsAndGoals);
     teamsGroupA.forEach(processGroupA);
-    
+
     teamsGroupB.forEach(calculatePointsGoalsMatches);
     teamsGroupB.sort(sortByPointsAndGoals);
     teamsGroupB.forEach(processGroupB);
