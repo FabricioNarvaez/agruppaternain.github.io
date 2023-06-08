432482b8ed1818ca0522c508f14442ce9cb0dc55
FabricioNarvaez <narvaezfabricio38@gmail.com>
2023-06-08 20:12:34 +0200

    Formated cookie.js with prettier

--- assets/js/cookie.js
+++ assets/js/cookie.js
@@ -1,39 +1,39 @@
-const cookieMessage = document.getElementById('cookie-message');
-const acceptCookiesBtn = document.getElementById('accept-cookies');
+const cookieMessage = document.getElementById("cookie-message");
+const acceptCookiesBtn = document.getElementById("accept-cookies");
 
 function setCookie(name, value, days) {
-  const expires = new Date();
-  expires.setTime(expires.getTime() + (days * 24 * 60 * 60 * 1000));
-  document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/`;
+    const expires = new Date();
+    expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
+    document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/`;
 }
 
 function getCookie(name) {
-  const cookieName = `${name}=`;
-  const cookies = document.cookie.split(';');
-  for (let i = 0; i < cookies.length; i++) {
-    let cookie = cookies[i];
-    while (cookie.charAt(0) === ' ') {
-      cookie = cookie.substring(1, cookie.length);
+    const cookieName = `${name}=`;
+    const cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i];
+        while (cookie.charAt(0) === " ") {
+            cookie = cookie.substring(1, cookie.length);
+        }
+        if (cookie.indexOf(cookieName) === 0) {
+            return cookie.substring(cookieName.length, cookie.length);
+        }
     }
-    if (cookie.indexOf(cookieName) === 0) {
-      return cookie.substring(cookieName.length, cookie.length);
-    }
-  }
-  return null;
+    return null;
 }
 
 function checkCookie() {
-  const cookieAccepted = getCookie('cookieAccepted');
-  if (!cookieAccepted) {
-    cookieMessage.classList.remove('hidden');
-  }
+    const cookieAccepted = getCookie("cookieAccepted");
+    if (!cookieAccepted) {
+        cookieMessage.classList.remove("hidden");
+    }
 }
 
-acceptCookiesBtn.addEventListener('click', () => {
-  setCookie('cookieAccepted', true, 30);
-  cookieMessage.style.display = 'none';
+acceptCookiesBtn.addEventListener("click", () => {
+    setCookie("cookieAccepted", true, 30);
+    cookieMessage.style.display = "none";
 });
 
-window.addEventListener('load', () => {
-  checkCookie();
+window.addEventListener("load", () => {
+    checkCookie();
 });
