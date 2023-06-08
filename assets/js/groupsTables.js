2deaf1e7b5ec0a0534715860b8de69d9bf8f6b3b
FabricioNarvaez <narvaezfabricio38@gmail.com>
2023-06-08 20:13:20 +0200

    Formated groupsTable.js with prettier

--- assets/js/groupsTables.js
+++ assets/js/groupsTables.js
@@ -1,13 +1,13 @@
 import { appendField, sortByGoals, appendSpan } from "./common.js";
-function createRow(team, index){
+function createRow(team, index) {
     let row = document.createElement("tr");
 
     const pos = index + 1;
     row.appendChild(appendField(pos));
-    if(pos <= 2){
-      row.children[0].classList.add("qualifiedToChampions");
-    }else{
-      row.children[0].classList.add("qualifiedToUEFA");
+    if (pos <= 2) {
+        row.children[0].classList.add("qualifiedToChampions");
+    } else {
+        row.children[0].classList.add("qualifiedToUEFA");
     }
 
     const teamField = document.createElement("td");
@@ -26,10 +26,10 @@ function createRow(team, index){
 
     row.appendChild(appendField(team.GD));
     row.children[8].style.fontWeight = "bold";
-    if(team.GD > 0){
-      row.children[8].style.color = "green";
-    }else if(team.GD < 0){
-      row.children[8].style.color = "red";
+    if (team.GD > 0) {
+        row.children[8].style.color = "green";
+    } else if (team.GD < 0) {
+        row.children[8].style.color = "red";
     }
     let pts = team.Pts;
     row.appendChild(appendField(pts));
@@ -41,16 +41,16 @@ function createRow(team, index){
 export function processGroupA(team, index) {
     let bodyTableA = document.getElementById("groupA");
     bodyTableA.appendChild(createRow(team, index));
-};
+}
 
 export function processGroupB(team, index) {
     let bodyTableB = document.getElementById("groupB");
     bodyTableB.appendChild(createRow(team, index));
-};
+}
 
-export function calculatePointsGoalsMatches(team){
+export function calculatePointsGoalsMatches(team) {
     let puntos = team.PG * 3;
-    team.Pts = team.PE ? (puntos + team.PE) : puntos;
+    team.Pts = team.PE ? puntos + team.PE : puntos;
 
     const gd = team.GF - team.GC;
     team.GD = gd > 0 ? `+${gd}` : gd;
@@ -60,10 +60,10 @@ export function calculatePointsGoalsMatches(team){
 
 export function sortByPointsAndGoals(a, b) {
     if (a.Pts > b.Pts) {
-      return -1;
+        return -1;
     } else if (a.Pts < b.Pts) {
-      return 1;
+        return 1;
     } else {
-      return sortByGoals(a, b);
+        return sortByGoals(a, b);
     }
 }
