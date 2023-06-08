8a375987d5b92483cd849ad03127689747f39bdf
FabricioNarvaez <narvaezfabricio38@gmail.com>
2023-06-08 20:16:20 +0200

    Formated validateToken.js with prettier

--- assets/js/validateToken.js
+++ assets/js/validateToken.js
@@ -1,22 +1,24 @@
-import { url } from './url.js';
-import { checkCookies } from './common.js';
+import { url } from "./url.js";
+import { checkCookies } from "./common.js";
 
 const urlToken = `${url}token`;
 let token = checkCookies("token=");
 
 fetch(urlToken, {
-  method: 'POST',
-  headers: {
-      'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({ token })
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ token }),
 })
-.then((response) => {
-  if (!response.ok) {
-    window.location.href = "login.html";
-  }else{
-    return response.json();
-  }
-})
-.then((data) => console.log(data))
-.catch((error) => console.error('There was an error with the request:', error));
+    .then((response) => {
+        if (!response.ok) {
+            window.location.href = "login.html";
+        } else {
+            return response.json();
+        }
+    })
+    .then((data) => console.log(data))
+    .catch((error) =>
+        console.error("There was an error with the request:", error)
+    );
