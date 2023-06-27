717e9290ea3d7137cf7b3741c9ec132abcbe310e
FabricioNarvaez <narvaezfabricio38@gmail.com>
2023-06-27 18:48:33 +0200

    Replaced admin-loader by camelcase

--- assets/js/validateToken.js
+++ assets/js/validateToken.js
@@ -3,7 +3,7 @@ import { checkCookies } from "./common.js";
 
 
 const adminContainer = document.getElementById("adminContainer");
-const adminLoader = document.getElementById("admin-loader");
+const adminLoader = document.getElementById("adminLoader");
 const urlToken = `${url}token`;
 let token = checkCookies("token=");
 
