23330a9c3ba3f540f005d28112d9d497f72fdc67
FabricioNarvaez <narvaezfabricio38@gmail.com>
2024-02-08 19:57:06 +0100

    Add main amazon page and create js with amazon products

---
+++ assets/js/amazon.js
@@ -0,0 +1,27 @@
+const amazonProducts = {
+    espinilleras: {
+        1: {
+            nombre: "NIKE Nk Merc Lite-Fa22",
+            imagen: "https://res.cloudinary.com/dzd68sxue/image/upload/v1707330218/Amazon/Espinilleras_Nike_ssaz4c.webp",
+            acercaDe: {
+                1: "El diseño anatómico está diseñado para adaptarse a los contornos de la espinilla para una amortiguación específica",
+                2: "La cubierta duradera está unida a una almohadilla de espuma densa para la difusión de impactos",
+                3: "Excelente amortiguación y absorción de impactos en un solo diseño"
+            },
+            precio: "19,99€ - 31,90€",
+            url: "https://www.amazon.es/NIKE-DN3611-010-NK-Merc-Lite/dp/B0B5HFVH7L/ref=sr_1_5?crid=1U7W3WXYGZY5N&amp;keywords=espinilleras%252Bde%252Bfutbol&amp;qid=1707326120&amp;sprefix=espinilleras%252Bde%252B%252Caps%252C213&amp;sr=8-5&amp;th=1&_encoding=UTF8&tag=fabricionarva-21&linkCode=ur2&linkId=ea8d8e887e31fc3033cf7f65e68fed38&camp=3638&creative=24630"
+        },
+        2: {
+            nombre: "Nike DN3614-113 Mercurial",
+            imagen: "https://res.cloudinary.com/dzd68sxue/image/upload/v1707333020/Amazon/Nike-Mercurial-_1__j3mynr.webp",
+            acercaDe: {
+                1: "Diseño ligero y ajuste regular",
+                2: "Ofrece comodidad y libertad de movimiento",
+                3: "Detalles distintivos de la marca"
+            },
+            precio: "15,35€",
+            url: "https://www.amazon.es/NIKE-DN3614-113-Mercurial-Hardshell-COCONUT/dp/B0B2K9WL2R/ref=pd_sbs_d_sccl_3_4/258-1359620-1181612?pd_rd_w=TQgr6&amp;content-id=amzn1.sym.5361c8f8-f11e-4bfa-91f1-8c7de1eb4cca&amp;pf_rd_p=5361c8f8-f11e-4bfa-91f1-8c7de1eb4cca&amp;pf_rd_r=HMS4379H2ZR9HC2VJZTF&amp;pd_rd_wg=MEsls&amp;pd_rd_r=03e1be47-5b2f-4342-a233-9557516f05e9&amp;pd_rd_i=B0B2K9WL2R&amp;psc=1&_encoding=UTF8&tag=fabricionarva-21&linkCode=ur2&linkId=d893e722cce90c93144bd708a9fca09b&camp=3638&creative=24630"
+        }
+    },
+
+}
