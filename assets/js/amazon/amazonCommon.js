4b21c4f1030faca76964ade073e099a0cdf2facb
FabricioNarvaez <narvaezfabricio38@gmail.com>
2024-02-10 12:39:58 +0100

    Load category for each amazon page

--- assets/js/amazon/amazonCommon.js
+++ assets/js/amazon/amazonCommon.js
@@ -1,9 +1,9 @@
 const amazonNavButtons = document.querySelector(".amazonNavButtons");
 
-function loadAmazonNavButtons(amazonProducts){
+function loadAmazonNavButtons(amazonProducts, folder){
     for(let category of amazonProducts){
         const navElement = document.createElement("a");
-        navElement.setAttribute("href", `amazonPages/${category.file}.html`);
+        navElement.setAttribute("href", `${folder}${category.file}.html`);
         navElement.innerHTML = category.category;
         amazonNavButtons.appendChild(navElement);
     }
