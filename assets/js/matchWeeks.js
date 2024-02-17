3514b514e408708f6f02d9eccd2e8396e8f4674e
FabricioNarvaez <narvaezfabricio38@gmail.com>
2024-02-17 13:31:53 +0100

    Publich matchweeks

--- assets/js/matchWeeks.js
+++ assets/js/matchWeeks.js
@@ -15,22 +15,17 @@ const matchWeeksLoader = document.getElementById("matchWeeksLoader");
     matchWeeksLoader.remove();
     const matchWeeksContent = document.getElementById("matchWeeksContent");
     if (matchWeeks.length) {
-        // TODO: Descomentar esto
-        // matchWeeks.forEach((matchWeek, index) => {
-        //     const matchWeekContainer = document.createElement("div");
-        //     matchWeekContainer.classList.add("tablesMargin");
-        //     const matchWeekTitle = document.createElement("h3");
-        //     matchWeekTitle.textContent = `${matchWeek.matchWeek} (${matchWeek.date})`;
+        matchWeeks.forEach((matchWeek, index) => {
+            const matchWeekContainer = document.createElement("div");
+            matchWeekContainer.classList.add("tablesMargin");
+            const matchWeekTitle = document.createElement("h3");
+            matchWeekTitle.textContent = `${matchWeek.matchWeek} (${matchWeek.date})`;
 
-        //     matchWeekContainer.appendChild(matchWeekTitle);
-        //     matchWeekContainer.appendChild(newDesign(matchWeek.matches));
+            matchWeekContainer.appendChild(matchWeekTitle);
+            matchWeekContainer.appendChild(newDesign(matchWeek.matches));
 
-        //     matchWeeksContent.appendChild(matchWeekContainer);
-        // });
-        // TODO: Borrar esto
-        const noData = document.createElement("h3");
-        noData.appendChild(appendField("Las jornadas aún no están definidas"));
-        matchWeeksContent.appendChild(noData);
+            matchWeeksContent.appendChild(matchWeekContainer);
+        });
     } else {
         const noData = document.createElement("h3");
         for (let i = 0; i < 4; i++) {
@@ -60,9 +55,10 @@ function createRow(matchData, hour) {
     const hourOrResult = matchData.localResult
         ? `${matchData.localResult} - ${matchData.visitorResult}`
         : `${hour}`;
+    const logo = matchData.localLogo ? matchData.localLogo : "https://res.cloudinary.com/dzd68sxue/image/upload/v1695396332/WEBP/default-bnoacd-1_qnmcps.webp";
     const template = `
         <div class="matchweekTeam">
-            <img src="${matchData.localLogo}" class="teamIndexLogo" alt="${matchData.local}" title="${matchData.local}">
+            <img src="${logo}" class="teamIndexLogo" alt="${matchData.local}" title="${matchData.local}">
             <p>${matchData.local}</p>
         </div>
         <div class="matchHourOrResult">
@@ -70,7 +66,7 @@ function createRow(matchData, hour) {
         </div>
         <div class="matchweekTeam matchweekTeamVisitor">
             <p>${matchData.visitor}</p>
-            <img src="${matchData.visitorLogo}" class="teamIndexLogo" alt="${matchData.visitorLogo}" title="${matchData.visitorLogo}">
+            <img src="${logo}" class="teamIndexLogo" alt="${matchData.visitorLogo}" title="${matchData.visitorLogo}">
         </div>
         `;
     return template;
